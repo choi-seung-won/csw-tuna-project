630fe4d2d842a19a6bf6c2185175d45bf04c518a
bccsw <fortheboost@naver.com>
2019-12-18 16:03:42 +0900

    project starts

---
+++ src/main/java/csw/tuna/project/App.java
@@ -0,0 +1,13 @@
+/*
+ * https://github.com/choi-seung-won/csw-tuna-project
+ */
+package csw.tuna.project;
+
+public class App {
+  
+    public static void main(String[] args) {
+//        System.out.println(new App().getGreeting());
+      
+      System.out.println("please sign in");
+    }
+}
